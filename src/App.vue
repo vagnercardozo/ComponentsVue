5a1884395d66027968bfdebc76887596fcf0d6c6
Eduardo da Silva <eduardo.silva@gmail.com>
2022-04-20 07:26:59 -0300

    Base para exercício de Vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+export default {
+  components: { MenuLateral },
+};
+</script>
+<template>
+  <MenuLateral />
+  <header id="header">Cabeçalho</header>
+  <main id="content">Principal</main>
+  <footer id="footer">Rodapé</footer>
+</template>
+<style></style>
